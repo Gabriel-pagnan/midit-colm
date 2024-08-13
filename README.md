e82767f51cf2cac67243706c2bd00e27b52c823f
Gabriel-pagnan <gabrielviniciuspagnan@gmail.com>
2024-08-13 18:47:23 -0300

    remove files

--- README.md
+++
@@ -1,1 +0,0 @@
-# midit-colm
--- api/routes/types/options.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class OptionType(BaseModel):
-    '''
-        ** Create model options in question
-    '''
-    yes_totally: bool
-    yes_needs_readjustment: bool
-    no: bool
